db27e6dc757eb0e719695f9a66053743a81eb616
leesj <kevinjun0913@gmail.com>
2024-06-20 15:56:02 +0900

    코드 백틱 보이는 문제 해결, 라이트 모드 버그 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,27 @@ import typography from "@tailwindcss/typography";
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        "code::before": {
+                            content: "&nbsp;&nbsp;",
+                        },
+                        "code::after": {
+                            content: "&nbsp;&nbsp;",
+                        },
+                        code: {
+                            background: "rgba(185, 185, 185, 0.2)",
+                            wordWrap: "break-word",
+                            boxDecorationBreak: "clone",
+                            padding: ".2rem .4rem",
+                            borderRadius: ".3rem",
+                        },
+                    },
+                },
+            },
+        },
         fontFamily: {
             Pretendard: ["Pretendard"],
         },
